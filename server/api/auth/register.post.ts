640237600989cc4226801b8a9715cc3805e335f1
Jan Fluksa <janfluksa@outlook.com>
2025-11-30 16:25:04 +0100

    Registration API and yup validation

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,16 +1,31 @@
 import { readBody } from 'h3'
 import bcrypt from 'bcrypt'
 import User from '~~/server/models/User' 
+import * as yup from 'yup'
+
+const registerSchema = yup.object({
+  firstname: yup.string().required('Firstname is required'),
+  lastname: yup.string().required('Lastname is required'),
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup.string().min(8, 'Password must be at least 8 chars long').required('Password is required')
+})
+
 
 export default defineEventHandler(async (event) => {
-  const { email, password, firstname, lastname } = await readBody(event)
+  const { email, password, firstname, lastname } = await readBody(event) || {}
 
-  if (!email || !password) {
+  
+
+  try {
+    await registerSchema.validate({email, password, firstname, lastname}, { abortEarly: false })
+  } catch (err) {
+    const errorMessage = err instanceof yup.ValidationError ? err.errors.join(', ') : 'Validation failed'
     throw createError({
       statusCode: 400,
-      statusMessage: 'Missing email or password'
+      statusMessage: errorMessage
     })
   }
+
 
   // 1) Zkontroluj, zda uživatel již existuje
   const existingUser = await User.findOne({ email })
